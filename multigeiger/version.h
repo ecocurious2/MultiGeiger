7abf8b6dd7126c5953e993466db838834739e6ce
Reinhard X. Fürst <rexfue@gmail.com>
2019-12-23 22:21:22 +0100

    Update version.h
    Added 4th Y in YYYY-MM-DD

--- multigeiger/version.h
+++ multigeiger/version.h
@@ -3,7 +3,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 11
 #define VERSION_PATCH 0
-// Date is in format "YYY-MM-DD".
+// Date is in format "YYYY-MM-DD".
 #define VERSION_DATE "2019-12-16"
 
 /**** Revision history:
