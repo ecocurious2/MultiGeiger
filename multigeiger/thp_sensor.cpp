0b7a18ae41d43034676b328bafc1f5f49b06753f
Thomas Waldmann <tw@waldmann-edv.de>
2020-02-21 10:38:54 +0100

    remove bme/BME from variable names, fixes #210

--- multigeiger/thp_sensor.cpp
+++ multigeiger/thp_sensor.cpp
@@ -8,26 +8,26 @@
 #include "log.h"
 #include "thp_sensor.h"
 
-int haveBME280 = 0;
+int have_thp = 0;
 
-float bme_temperature = 0.0;
-float bme_humidity = 0.0;
-float bme_pressure = 0.0;
+float temperature = 0.0;
+float humidity = 0.0;
+float pressure = 0.0;
 
 Adafruit_BME280 bme;
 
 void setup_thp_sensor() {
-  haveBME280 = bme.begin(BME280_ADDRESS);
-  if (!haveBME280)
-    haveBME280 = bme.begin(BME280_ADDRESS_ALTERNATE);
-  log(INFO, "BME_Status: %d  ID:%0X", haveBME280, bme.sensorID());
+  have_thp = bme.begin(BME280_ADDRESS);
+  if (!have_thp)
+    have_thp = bme.begin(BME280_ADDRESS_ALTERNATE);
+  log(INFO, "BME_Status: %d  ID:%0X", have_thp, bme.sensorID());
 }
 
 void read_thp_sensor() {
-  if (haveBME280) {
-    bme_temperature = bme.readTemperature();
-    bme_humidity = bme.readHumidity();
-    bme_pressure = bme.readPressure();
+  if (have_thp) {
+    temperature = bme.readTemperature();
+    humidity = bme.readHumidity();
+    pressure = bme.readPressure();
   }
 }
 
