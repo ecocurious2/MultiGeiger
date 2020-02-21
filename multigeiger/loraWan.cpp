97e5f1fa7266440e8620b4e29db6a04214bbc35c
Thomas Waldmann <tw@waldmann-edv.de>
2020-02-21 11:42:54 +0100

    loraWan: cleaned up comments / style, fixes #155

--- multigeiger/loraWan.cpp
+++ multigeiger/loraWan.cpp
@@ -1,4 +1,6 @@
 // loraWan hardware related code
+// code based on free-to-use / do-anything-what-you-want-with-it example code
+// by Thomas Telkamp, Matthijs Kooijman and Terry Moore, MCCI.
 
 // IOs for optional LoRa
 //
@@ -22,44 +24,25 @@
 #endif
 
 #if SEND2LORA
-/*******************************************************************************
- * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
- * Copyright (c) 2018 Terry Moore, MCCI
- *
- * Permission is hereby granted, free of charge, to anyone
- * obtaining a copy of this document and accompanying files,
- * to do whatever they want with them without any restriction,
- * including, but not limited to, copying, modification and redistribution.
- * NO WARRANTY OF ANY KIND IS PROVIDED.
- *
- * This example sends a valid LoRaWAN packet with payload "Hello,
- * world!", using frequency and encryption settings matching those of
- * the The Things Network.
- *
- * This uses OTAA (Over-the-air activation), where where a DevEUI and
- * application key is configured, which are used in an over-the-air
- * activation procedure where a DevAddr and session keys are
- * assigned/generated for use with all further communication.
- *
- * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
- * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
- * violated by this sketch when left running for longer)!
+// Send a valid LoRaWAN packet using frequency and encryption settings matching
+// those of the The Things Network.
+//
+// This uses OTAA (Over-the-air activation), where where a DevEUI and
+// application key is configured, which are used in an over-the-air
+// activation procedure where a DevAddr and session keys are
+// assigned/generated for use with all further communication.
+//
+// Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+// g1, 0.1% in g2), but not the TTN fair usage policy.
+//
+// To use this code, first register your application and device with
+// the things network, to set or generate an AppEUI, DevEUI and AppKey.
+// Multiple devices can use the same AppEUI, but each device has its own
+// DevEUI and AppKey.
+//
+// Do not forget to define the radio type correctly in
+// arduino-lmic/project_config/lmic_project_config.h or from your BOARDS.txt.
 
- * To use this sketch, first register your application and device with
- * the things network, to set or generate an AppEUI, DevEUI and AppKey.
- * Multiple devices can use the same AppEUI, but each device has its own
- * DevEUI and AppKey.
- *
- * Do not forget to define the radio type correctly in
- * arduino-lmic/project_config/lmic_project_config.h or from your BOARDS.txt.
- *
- *******************************************************************************/
-// Ensure the setting is correct in the LMIC config file located in
-// LMIC library/project_config/lmic_project_config.h
-// These two defines have no more effect than creating an error if the library setting
-// is not correct
-//#define CFG_eu868 1
-//#define CFG_sx1276_radio 1
 #include <Arduino.h>
 #include <lmic.h>
 #include <hal/hal.h>
@@ -69,13 +52,11 @@
 #include "loraWan.h"
 #include "log.h"
 
-//
 // For normal use, we require that you edit the sketch to replace FILLMEIN
 // with values assigned by the TTN console. However, for regression tests,
 // we want to be able to compile these scripts. The regression tests define
 // COMPILE_REGRESSION_TEST, and in that case we define FILLMEIN to a non-
 // working but innocuous value.
-//
 #ifdef COMPILE_REGRESSION_TEST
 # define FILLMEIN 0
 #else
@@ -85,8 +66,7 @@
 
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from ttnctl output, this means to reverse
-// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
-// 0x70.
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3, 0x70.
 // static const u1_t PROGMEM APPEUI[8]={ 0x45, 0xB6, 0x01, 0xD0, 0x7E, 0xD5, 0xB3, 0x70};
 static const u1_t PROGMEM APPEUI[8]= { 0x98, 0x7A, 0x01, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
@@ -105,8 +85,7 @@ void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
 static osjob_t sendjob;
 
-// Schedule TX every this many seconds (might become longer due to duty
-// cycle limitations).
+// Schedule TX every this many seconds (might become longer due to duty cycle limitations).
 const unsigned TX_INTERVAL = 10;
 
 // Pin mapping
@@ -171,14 +150,11 @@ void onEvent(ev_t ev) {
             // size, we don't use it in this example.
             LMIC_setLinkCheckMode(0);
             break;
-        /*
-        || This event is defined but not used in the code. No
-        || point in wasting codespace on it.
-        ||
-        || case EV_RFU1:
-        ||     event_log("EV_RFU1");
-        ||     break;
-        */
+        // This event is defined but not used in the code.
+        // No point in wasting codespace on it.
+        // case EV_RFU1:
+        //   event_log("EV_RFU1");
+        //   break;
         case EV_JOIN_FAILED:
             txStatus = TX_STATUS_ENDING_ERROR;
             event_log("EV_JOIN_FAILED");
@@ -225,14 +201,11 @@ void onEvent(ev_t ev) {
             txStatus = TX_STATUS_UNKNOWN;
             event_log("EV_LINK_ALIVE");
             break;
-        /*
-        || This event is defined but not used in the code. No
-        || point in wasting codespace on it.
-        ||
-        || case EV_SCAN_FOUND:
-        ||    event_log("EV_SCAN_FOUND");
-        ||    break;
-        */
+        // This event is defined but not used in the code.
+        // No point in wasting codespace on it.
+        // case EV_SCAN_FOUND:
+        //   event_log("EV_SCAN_FOUND");
+        //   break;
         case EV_TXSTART:
             txStatus = TX_STATUS_UNKNOWN;
             event_log("EV_TXSTART");
@@ -245,9 +218,6 @@ void onEvent(ev_t ev) {
 }
 
 
-/**
- * Setup the LoRaWan stack for TTN Europe
- */
 void setup_lorawan() {
     txStatus = TX_STATUS_UNKNOWN;
     // LMIC init
@@ -256,33 +226,32 @@ void setup_lorawan() {
     LMIC_reset();    
     LMIC_setClockError(MAX_CLOCK_ERROR * 1 / 100);
 
-    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
-    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // Setup the LoRaWan stack for TTN Europe
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);  // g-band
+    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);  // g-band
+    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);  // g-band
+    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);  // g-band
+    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);  // g-band
+    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);  // g-band
+    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);  // g-band
+    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);  // g-band
+    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);  // g2-band
     LMIC_setLinkCheckMode(0);
     LMIC.dn2Dr = SF9;
     LMIC_setDrTxpow(DR_SF7,14);
 }
 
-/**
- * Send LoRaWan frame with ack or not
- * - txPort : port to transmit 
- * - txBuffer : message to transmit
- * - txSz : size of the message to transmit
- * - ack : true for message ack & downlink / false for pure uplink
- *   When Ack is false, the downlink buffer can be set to NULL as rxSz and rPort
- * - rxPort : where to write the port where downlink has been received
- * - rxBuffer : where the downlinked data will be stored
- * - rxSz : size of received data
- */
+// Send LoRaWan frame with ack or not
+// - txPort : port to transmit 
+// - txBuffer : message to transmit
+// - txSz : size of the message to transmit
+// - ack : true for message ack & downlink / false for pure uplink
+//   When Ack is false, the downlink buffer can be set to NULL as rxSz and rPort
+// - rxPort : where to write the port where downlink has been received
+// - rxBuffer : where the downlinked data will be stored
+// - rxSz : size of received data
 transmissionStatus_t lorawan_send(uint8_t txPort, uint8_t * txBuffer, uint8_t txSz, bool ack, uint8_t * rxPort, uint8_t * rxBuffer, uint8_t * rxSz){
-  // Check if there is not a current TX/RX job running
+    // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
         log(DEBUG, "OP_TXRXPEND, not sending");
         return TX_STATUS_ENDING_ERROR;
