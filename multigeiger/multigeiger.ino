4af297ae236c832d0137a31b0caa09fb4be87835
Thomas Waldmann <tw@waldmann-edv.de>
2020-02-22 21:43:10 +0100

    replace long license legalese by a shorter version.

--- multigeiger/multigeiger.ino
+++ multigeiger/multigeiger.ino
@@ -1,19 +1,6 @@
 // Project: Simple Multi-Geiger
 // (c) 2019,2020 by the authors, see AUTHORS file in toplevel directory.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-// (see LICENSE file in toplevel directory)
+// Licensed under the GPL v3 (or later), see LICENSE file in toplevel directory.
 //
 // Description: With minimal external components you are able to build a Geiger Counter that:
 //   - is precise
