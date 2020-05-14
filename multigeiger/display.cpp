b47c11ac744fb94a498f0b74059f1c4febb3f310
Thomas Waldmann <tw@waldmann-edv.de>
2020-05-14 17:02:31 +0200

    remove "cpm" text output, fixes #298
    this gets overwritten by the status display on the smaller display.
    
    on the larger display we do not output "cpm" as text.

--- multigeiger/display.cpp
+++ multigeiger/display.cpp
@@ -173,8 +173,6 @@ void DisplayGMC(int TimeSec, int RadNSvph, int CPM, bool use_display) {
     pu8x8->setFont(u8x8_font_px437wyse700b_2x2_f);
     sprintf(output, "%4d", CPM);
     pu8x8->drawString(0, 3, output);
-    pu8x8->setFont(u8x8_font_amstrad_cpc_extended_f);
-    pu8x8->drawString(0, 5, "     cpm");
   }
   displayStatus();
   displayIsClear = false;
