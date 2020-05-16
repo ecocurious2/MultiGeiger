e5d7c1c784aa064006d3a5ab555a318287a4e541
Thomas Waldmann <tw@waldmann-edv.de>
2020-05-16 21:04:13 +0200

    Bump version: 1.14.0 → 1.15.0-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, see AUTHORS'
 author = 'see AUTHORS'
 
 # The full version, including alpha/beta/rc tags
-release = 'V1.14.0'
+release = 'V1.15.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
